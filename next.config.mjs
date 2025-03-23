28a9fa6b04bef0e80530328dd36fb4da1dcd0820
ramicoin <ramicoinbsc@gmail.com>
2025-03-23 15:30:49 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+};
+
+export default nextConfig;
